07c1e763fdfea42011c75f900efaaee689a5a285
Boris Mavrin <genmavr@gmail.com>
2017-02-22 00:34:38 +0200

    for heroku

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ var {
     Todo
 } = require('./models/todo')
 
+const port= process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -54,8 +56,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Started on port 3000");
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 module.exports = {
     app
